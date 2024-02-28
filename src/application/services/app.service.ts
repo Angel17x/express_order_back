ad1cab66f1624378aed053dcf534343e1dd68089
Angel Lugo <ange26078613@gmail.com>
2024-02-27 20:48:13 -0400

    add / route

--- src/application/services/app.service.ts
+++ src/application/services/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
-
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { Response, response } from 'express'; 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  find(): Response<object> {
+    return (
+      response.status(HttpStatus.OK)
+      .json(
+        { 
+          message: 'Welcome to the express order api', 
+          status: HttpStatus.OK 
+        }
+      )
+    );
   }
 }
