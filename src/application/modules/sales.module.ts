602a906370c96056e4154ea7a3bf18123a1a5093
Angel Lugo <ange26078613@gmail.com>
2024-03-01 17:27:13 -0400

    changes

--- src/application/modules/sales.module.ts
+++ src/application/modules/sales.module.ts
@@ -2,12 +2,14 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthMiddleware } from '../middlewares/auth.middleware';
 import { DatabaseModule } from './database.module';
 import { SalesController } from '../controllers/sales.controller';
+import { JwtServiceImpl } from '../services/jwt.service.impl';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [SalesController],
   providers: [
-    AuthMiddleware
+    AuthMiddleware,
+    JwtServiceImpl
   ],
 })
 export class SalesModule implements NestModule {
