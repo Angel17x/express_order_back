d58e6e09ec09553cd0348ac5da7bc24724f57ca8
Angel Lugo <ange26078613@gmail.com>
2024-03-01 17:13:55 -0400

    changes

--- src/domain/schemas/schemas.ts
+++ src/domain/schemas/schemas.ts
@@ -1,9 +1,11 @@
 import { ProductSchema } from './Product.schema';
+import { SalesSchema } from './sales.schema';
 import { UserSchema } from './user.schema';
 
 const schemas = [
   { name: 'User', schema: UserSchema },
   { name: 'Product', schema: ProductSchema },
+  { name: 'Sales', schema: SalesSchema },
   // ...otros esquemas
 ];
 
